903dab9de23a0db46b2ec1291317e93f4b5c3d6b
Spencer Anderson <spenceranderson007@gmail.com>
2022-09-14 12:33:20 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const cors = require('cors')
+const Sequelize = require('sequelize')
+const sequelize = require('sequelize')
+require('dotenv').consig()
+
+// const sequelize = new Sequelize(CONNECTION_STRING, {
+//     dialect: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             rejectUnauthorized: false,
+//         }
+//     }
+// })
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+//endpoints
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/styles.css"))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.js"))
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./server/server.js"))
+})
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
